9fcdb5951436a8d51d3b0541ccbd2b8deb51dca6
marina <greenbox22@yandex.ru>
2024-02-02 01:40:26 +0400

    начала DeserializingAnimalArray

--- src/DeserializingAnimalArray.java
+++ src/DeserializingAnimalArray.java
@@ -1,2 +1,28 @@
+import java.io.Serializable;
+import java.util.Objects;
+
 public class DeserializingAnimalArray {
+    public static void main(String[] args) {
+
+    }
+
+    class Animal implements Serializable {
+        private final String name;
+
+        public Animal(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Animal) {
+                return Objects.equals(name, ((Animal) obj).name);
+            }
+            return false;
+        }
+    }
+    public static Animal[] deserializeAnimalArray(byte[] data) {
+        // your implementation here
+        return null;
+    }
 }
