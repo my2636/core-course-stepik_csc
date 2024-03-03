1cf9aa38e96141955151c7365c8688afa1405dea
marina <greenbox22@yandex.ru>
2024-03-03 15:15:18 +0400

    pseudoRandomStream finished

--- src/generics_collections_streams/PseudoRandomStream.java
+++ src/generics_collections_streams/PseudoRandomStream.java
@@ -4,10 +4,15 @@ import java.util.stream.IntStream;
 
 public class PseudoRandomStream {
     public static void main(String[] args) {
+        int a = 13;
 
+        pseudoRandomStream(a);
     }
 
     public static IntStream pseudoRandomStream(int seed) {
-        return null;
+/*
+        IntStream.iterate(seed, x -> (((x * x) / 10)) % 1000 ).forEach(System.out::println);
+*/
+        return  IntStream.iterate(seed, x -> (((x * x) / 10)) % 1000 );
     }
 }
